3be2bf83accec683502c2d617fde3cbecdf322a4
Dmitriy_Sankevich <Dmitriy_Sankevich@epam.com>
2018-01-08 18:03:05 +0600

    Make list work

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -3,20 +3,58 @@ import React, { Component } from 'react';
 class TaskItem extends Component{
     
     constructor(props){
-        super(props)
+        super(props);
+
+        this.state = {
+                    value: this.props.task,
+                    editMode:false,
+                    editButtonValue: "Edit"
+                };
+
+        this.editButtonHandler = this.editButtonHandler.bind(this);
+        this.deleteButtonHandler = this.deleteButtonHandler.bind(this);
+        this.onChangeValue = this.onChangeValue.bind(this);
     }
+
+    editButtonHandler(e){
+        let state = null;
+        if(this.state.editMode){
+            state = {
+                        editMode: false,
+                        editButtonValue: "Edit"
+                    };
+        }else{
+            state = {
+                        editMode: true,
+                        editButtonValue: "Save"
+                    };
+        }
+        this.setState(state);
+    }
+
+    deleteButtonHandler(e){
+        alert("Delete");
+    }
+
+    onChangeValue(e){
+        this.setState({value:e.target.value});
+    }
+
 
     render(){
         return <tr>
-                <td><input type='text' value={this.props.task}/></td>
-                <td><input type='button' value='Edit'/></td>
-                <td><input type='button' value='Delete'/></td>
+                <td>
+                    { 
+                        (this.state.editMode) 
+                        ? <input type='text' value={this.state.value} onChange={this.onChangeValue}/> 
+                        : this.state.value 
+                    }
+                </td>
+
+                <td><input onClick={this.editButtonHandler} type='button' value={this.state.editButtonValue}/></td>
+                <td><input onClick={this.deleteButtonHandler} type='button' value='Delete'/></td>
             </tr>;
     }
 }
-
-
-
-
 
 export default TaskItem;
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -6,15 +6,14 @@ class TaskList extends Component {
     
     constructor(props){
         super(props);
-        //this.setState({List:[100, 200, 300]});
         this.state = { List: this.props.List };
     }
     
     render(){
-// let ss = this.state.List.map((current, index) => <TaskItem key={index} task={current}/>);
+        const items = this.state.List.map((current, index) => <TaskItem key={index} task={current}/>);
         return <table>
             <tbody>
-                {this.state.List.map((current, index) => <TaskItem key={index} task={current}/>)}
+                {items}
             </tbody>
         </table>;
     }
